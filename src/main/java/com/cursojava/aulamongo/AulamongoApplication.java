f727cc58cdfde361c8b2e1b525d147bcb29e1cb9
Marcelo Ishii <marcelokoiti2014@gmail.com>
2021-03-23 07:28:58 -0300

    primeiro commit - projeto criado

---
+++ src/main/java/com/cursojava/aulamongo/AulamongoApplication.java
@@ -0,0 +1,13 @@
+package com.cursojava.aulamongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulamongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulamongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursojava/aulamongo/AulamongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursojava.aulamongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulamongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
