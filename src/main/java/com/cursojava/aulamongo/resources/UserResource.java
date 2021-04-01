a688e7ff8c9c7b06f47b92714a1a5b8dd0c644c2
Marcelo Ishii <marcelokoiti2014@gmail.com>
2021-04-01 08:00:24 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/cursojava/aulamongo/resources/UserResource.java
+++ src/main/java/com/cursojava/aulamongo/resources/UserResource.java
@@ -53,7 +53,7 @@ public class UserResource {
 	@RequestMapping(value="/{id}",method=RequestMethod.PUT)
 	public ResponseEntity<Void> Update(@RequestBody UserDTO objDto,@PathVariable String id ) {
 		User obj = service.fromDTO(objDto);              // Converte UserDto para User
-		obj.setId(id);                                   // Por que nao pegar o que esta em objDto ?
+		obj.setId(id);                                  
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
