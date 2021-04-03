c1d77a92e58f76cdaa43d48488ca984a90b772ed
Marcelo Ishii <marcelokoiti2014@gmail.com>
2021-04-03 11:15:14 -0300

    Obtendo um post por id

---
+++ src/main/java/com/cursojava/aulamongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.cursojava.aulamongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursojava.aulamongo.domain.Post;
+import com.cursojava.aulamongo.repository.PostRepository;
+import com.cursojava.aulamongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	//Instancia objeto do tipo UserRepository. 
+	//@Autowired: o Spring instancia automatico (Injecao de dependencia)	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto Post não encontrado"));
+	}
+}
