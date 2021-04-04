ed00ac937e5c78683b35e6654f107c1c62319fbd
Marcelo Ishii <marcelokoiti2014@gmail.com>
2021-04-04 10:50:24 -0300

    Consulta simples com query methods

--- src/main/java/com/cursojava/aulamongo/repository/PostRepository.java
+++ src/main/java/com/cursojava/aulamongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.cursojava.aulamongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,10 @@ import com.cursojava.aulamongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	//Da classe Post, no  atributo title
+	//Automaticamente o Spring Data monta a consulta
+//	List<Post> findByTitleContains(String text);
+
+	//Ignora Maiuscula/minuscula
+	List<Post> findByTitleContainsIgnoreCase(String text);
 }
--- src/main/java/com/cursojava/aulamongo/resources/PostResource.java
+++ src/main/java/com/cursojava/aulamongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.cursojava.aulamongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursojava.aulamongo.domain.Post;
+import com.cursojava.aulamongo.resources.util.URL;
 import com.cursojava.aulamongo.services.PostService;
 
 @RestController
@@ -22,5 +26,17 @@ public class PostResource {
 		Post obj = service.findById(id);                 // Retorna Post
 		
 		return ResponseEntity.ok().body(obj);
+	}
+
+	// O valor a ser pesquisado sera passado como parametro
+	// value="text" é o nome do parametro passado
+	@RequestMapping(value="/titlesearch",method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		// decode do parametro
+		text = URL.decodeParam(text);
+		
+		List<Post> list = service.findByTitle(text);
+		
+		return ResponseEntity.ok().body(list);
 	}
 }
