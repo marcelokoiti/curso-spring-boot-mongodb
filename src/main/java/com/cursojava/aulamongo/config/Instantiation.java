7360492ee2a7bbda907ccd23cc8093706a5bd1f1
Marcelo Ishii <marcelokoiti2014@gmail.com>
2021-04-04 08:41:41 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/cursojava/aulamongo/config/Instantiation.java
+++ src/main/java/com/cursojava/aulamongo/config/Instantiation.java
@@ -1,9 +1,7 @@
 package com.cursojava.aulamongo.config;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.cursojava.aulamongo.domain.Post;
 import com.cursojava.aulamongo.domain.User;
 import com.cursojava.aulamongo.dto.AuthorDTO;
+import com.cursojava.aulamongo.dto.CommentDTO;
 import com.cursojava.aulamongo.repository.PostRepository;
 import com.cursojava.aulamongo.repository.UserRepository;
 
@@ -40,7 +39,14 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
-	
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		//addAll - faz append dos elementos na List  
